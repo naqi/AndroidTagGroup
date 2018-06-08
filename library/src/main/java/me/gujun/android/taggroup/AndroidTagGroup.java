4ec9a10abbd961bc67b3372526a2fb9c17080001
Naqi Syed <naqisyed@gmail.com>
2018-06-08 15:46:43 -0500

    Revert "moved checked code TagView init"
    This reverts commit e123eddf1cfc670447f1be8d898b6815a4fd6f39.
    
    Signed-off-by: Naqi Syed <naqisyed@gmail.com>

--- library/src/main/java/me/gujun/android/taggroup/AndroidTagGroup.java
+++ library/src/main/java/me/gujun/android/taggroup/AndroidTagGroup.java
@@ -756,6 +756,7 @@ public class AndroidTagGroup extends ViewGroup {
     public void appendTag(CharSequence tag) {
         final TagView newTag = new TagView(getContext(), TagView.STATE_NORMAL, tag);
         newTag.setOnClickListener(mInternalTagClickListener);
+        newTag.setChecked(mIsAppendMode);
         addView(newTag);
     }
 
@@ -995,7 +996,6 @@ public class AndroidTagGroup extends ViewGroup {
             mState = state;
 
             setClickable(mIsAppendMode);
-            setChecked(mIsAppendMode);
             setFocusable(state == STATE_INPUT);
             setFocusableInTouchMode(state == STATE_INPUT);
             setHint(state == STATE_INPUT ? mInputHint : null);
