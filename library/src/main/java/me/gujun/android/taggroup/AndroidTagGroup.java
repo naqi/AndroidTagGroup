d3ffa57f1f44e7bc4d922c4612054e4aa13b2ce3
Naqi Syed <naqisyed@gmail.com>
2018-06-08 15:46:56 -0500

    Revert "When in append mode tags are also checked"
    This reverts commit 5427b12d86f355f9826d773d3a99431261ea5f19.
    
    Signed-off-by: Naqi Syed <naqisyed@gmail.com>

--- library/src/main/java/me/gujun/android/taggroup/AndroidTagGroup.java
+++ library/src/main/java/me/gujun/android/taggroup/AndroidTagGroup.java
@@ -756,7 +756,6 @@ public class AndroidTagGroup extends ViewGroup {
     public void appendTag(CharSequence tag) {
         final TagView newTag = new TagView(getContext(), TagView.STATE_NORMAL, tag);
         newTag.setOnClickListener(mInternalTagClickListener);
-        newTag.setChecked(mIsAppendMode);
         addView(newTag);
     }
 
